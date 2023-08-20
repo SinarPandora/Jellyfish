dba86bc42f003ffff591863b3bf3485d1a527d35
Sinar Pandora <SinarPandora@outlook.com>
2023-08-20 17:37:28 +0800

    Add migration

---
+++ Jellyfish/Migrations/20230820093421_CreateTpConfigTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTpConfigTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "team_play_config",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    channel_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    create_time = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp"),
+                    update_time = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_team_play_config", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "team_play_room_instance",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    tp_config_id = table.Column<long>(type: "bigint", nullable: false),
+                    voice_channel_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    creator_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    member_limit = table.Column<long>(type: "bigint", nullable: false),
+                    create_time = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp"),
+                    update_time = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_team_play_room_instance", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_team_play_room_instance_team_play_config_tp_config_id",
+                        column: x => x.tp_config_id,
+                        principalTable: "team_play_config",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_team_play_room_instance_tp_config_id",
+                table: "team_play_room_instance",
+                column: "tp_config_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "team_play_room_instance");
+
+            migrationBuilder.DropTable(
+                name: "team_play_config");
+        }
+    }
+}
