645e75520163bfb1be61c4a2a377032714855dd6
Sinar Pandora <SinarPandora@outlook.com>
2023-08-28 00:37:21 +0800

    Enable debug command only in debug build

--- Jellyfish/src/Module/SimpleTestCommand.cs
+++ Jellyfish/src/Module/SimpleTestCommand.cs
@@ -1,6 +1,6 @@
+using System.Diagnostics;
 using Jellyfish.Core.Command;
 using Jellyfish.Core.Kook;
-using Kook;
 using Kook.WebSocket;
 using NLog;
 
@@ -9,7 +9,7 @@ namespace Jellyfish.Module;
 /// <summary>
 ///     Simple testing command
 /// </summary>
-public class SimpleTestCommand : IMessageCommand
+public class SimpleTestCommand : MessageCommand
 {
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
     private readonly KookApiFactory _apiFactory;
@@ -17,21 +17,34 @@ public class SimpleTestCommand : IMessageCommand
     public SimpleTestCommand(KookApiFactory apiFactory)
     {
         _apiFactory = apiFactory;
+        Enabled = false;
+        EnableOnlyOnDebug();
     }
 
-    public string Name()
-    {
-        return "简单测试指令";
-    }
+    public override string Name() => "简单测试指令";
 
-    public async Task<CommandResult> Execute(SocketMessage msg, SocketGuildUser user, SocketTextChannel channel)
+    public override string[] Keywords() => new[] { "!Test" };
+
+    public override string Help() => throw new NotSupportedException("测试指令不包含帮助信息也不应该对用户开放");
+
+    public override async Task<CommandResult> Execute(SocketMessage msg, SocketGuildUser user,
+        SocketTextChannel channel)
     {
-        if (msg is not { Type: MessageType.KMarkdown, Content: "！Test" }) return CommandResult.Continue;
+        if (msg.Content != "!Test" && msg.Content != "！Test") return CommandResult.Continue;
 
         await channel.SendTextAsync("Simple hello world!");
 
         Log.Info($"Current Boot Level is {channel.Guild.BoostLevel}");
 
         return CommandResult.Done;
+    }
+
+    /// <summary>
+    ///     Enable this command only on debug
+    /// </summary>
+    [Conditional("DEBUG")]
+    private void EnableOnlyOnDebug()
+    {
+        Enabled = true;
     }
 }
