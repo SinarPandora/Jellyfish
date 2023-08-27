67d0fcc582304ef82374c66f6b1222f203b87c7d
Sinar Pandora <SinarPandora@outlook.com>
2023-08-27 21:35:45 +0800

    Move folder

---
+++ Jellyfish/Migrations/20230827133530_MoveEntitiesPathTwice.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveEntitiesPathTwice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
