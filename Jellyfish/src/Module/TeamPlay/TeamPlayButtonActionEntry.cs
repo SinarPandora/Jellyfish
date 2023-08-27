f9f059857865863815eedbaf3b9baa32e3d048d7
Sinar Pandora <SinarPandora@outlook.com>
2023-08-28 00:36:52 +0800

    Merge command action and split into 2 for permission control

--- Jellyfish/src/Module/TeamPlay/TeamPlayButtonActionEntry.cs
+++ Jellyfish/src/Module/TeamPlay/TeamPlayButtonActionEntry.cs
@@ -1,28 +1,73 @@
 using Jellyfish.Core.Command;
+using Jellyfish.Core.Data;
+using Jellyfish.Module.TeamPlay.Data;
+using Jellyfish.Util;
 using Kook;
 using Kook.WebSocket;
+using NLog;
+using VoiceQuality = Jellyfish.Core.Kook.Protocol.VoiceQuality;
 
 namespace Jellyfish.Module.TeamPlay;
 
 /// <summary>
 ///     Team play card action
 /// </summary>
-public class TeamPlayButtonActionEntry : IButtonActionCommand
+public class TeamPlayButtonActionEntry : ButtonActionCommand
 {
-    public string Name()
-    {
-        return "组队游戏卡片操作";
-    }
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
-    public async Task<CommandResult> Execute(string value, Cacheable<SocketGuildUser, ulong> user,
+    public override string Name() => "组队游戏卡片操作";
+
+    public override async Task<CommandResult> Execute(string value, Cacheable<SocketGuildUser, ulong> user,
         Cacheable<IMessage, Guid> message, SocketTextChannel channel)
     {
         if (value.StartsWith("tp_binding_"))
         {
-            await TeamPlayManagerAction.DoBindingParentChannel(value[11..], user, channel);
+            await BindingParentChannel(value[11..], user, channel);
             return CommandResult.Done;
         }
 
         return CommandResult.Continue;
+    }
+
+    private static async Task BindingParentChannel(string name, Cacheable<SocketGuildUser, ulong> user,
+        SocketTextChannel channel)
+    {
+        Log.Info($"已收到名为 {name} 的绑定请求，执行进一步操作");
+        var voiceChannel = user.Value.VoiceChannel;
+        if (voiceChannel == null)
+        {
+            await channel.SendErrorCardAsync("未检测到您加入的语音频道");
+        }
+        else
+        {
+            Log.Info($"已检测到语音频道：{voiceChannel.Name}：{voiceChannel.Id}");
+            await channel.SendInfoCardAsync($"检测到您加入了频道：{voiceChannel.Name}，正在绑定...");
+
+
+            await using var dbCtx = new DatabaseContext();
+            var record = dbCtx.TpConfigs
+                .FirstOrDefault(e => e.Name == name);
+
+            // Update or Insert
+            if (record != null)
+            {
+                record.VoiceQuality = channel.Guild.BoostLevel > BoostLevel.None
+                    ? VoiceQuality.High
+                    : VoiceQuality.Medium;
+                record.VoiceChannelId = voiceChannel.Id;
+            }
+            else
+            {
+                record = new TpConfig(name, voiceChannel.Id, channel.Guild.Id);
+                dbCtx.TpConfigs.Add(record);
+            }
+
+            dbCtx.SaveChanges();
+
+            await channel.SendSuccessCardAsync("绑定成功！");
+
+            Log.Info($"成功绑定 {name} 到 {voiceChannel.Name}：{voiceChannel.Id}，ID：{record.Id}");
+        }
     }
 }
---
+++ Jellyfish/src/Module/TeamPlay/TeamPlayManageCommand.cs
@@ -0,0 +1,180 @@
+using Jellyfish.Core.Command;
+using Jellyfish.Core.Data;
+using Jellyfish.Core.Kook.Protocol;
+using Jellyfish.Util;
+using Kook;
+using Kook.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using NLog;
+using AppCtx = Jellyfish.Core.Container.AppContext;
+using VoiceQuality = Jellyfish.Core.Kook.Protocol.VoiceQuality;
+
+namespace Jellyfish.Module.TeamPlay;
+
+/// <summary>
+///     Team play entry command
+/// </summary>
+public class TeamPlayManageCommand : MessageCommand
+{
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public override string Name() => "管理组队配置指令";
+
+    public override string[] Keywords() => new[] { "!组队" };
+
+    public override string Help() =>
+        """
+        组队系统：管理员指令
+        指令名称：！组队
+
+        参数：
+        帮助：显示此消息
+        列表：列出全部的组队频道配置
+        绑定 [名称]：开始组队功能绑定
+        绑定文字频道 [名称]：在目标频道中使用，设置后，该频道发送的组队质量会使用该配置创建语音频道
+        语音质量 [配置 ID] [低|中|高]：设定临时语音频道的质量，配置 ID 可以通过“列表”指令获取
+        """;
+
+    public override async Task<CommandResult> Execute(SocketMessage msg, SocketGuildUser user,
+        SocketTextChannel channel)
+    {
+        if (!msg.Content.StartsWith("！组队") && !msg.Content.StartsWith("!组队")) return CommandResult.Continue;
+
+        var args = msg.Content[3..].TrimStart();
+        if (args.StartsWith("帮助"))
+            await channel.SendTextAsync(Help());
+        else if (args.StartsWith("绑定"))
+            await StartBindingParentChannel(channel, args[2..].TrimStart());
+        else if (args.StartsWith("绑定文字频道"))
+            await BindingTextChannel(channel, args[6..].TrimStart());
+        else if (args.StartsWith("语音质量"))
+            await SetDefaultQuality(channel, args[6..].Trim());
+        else if (args.StartsWith("列表"))
+            await ListConfigs(channel);
+        else
+            await channel.SendTextAsync(Help());
+
+        return CommandResult.Done;
+    }
+
+    private static async Task StartBindingParentChannel(SocketTextChannel channel, string name)
+    {
+        await using var dbCtx = new DatabaseContext();
+
+        var names = dbCtx.TpConfigs.AsNoTracking()
+            .Select(c => c.Name)
+            .OrderBy(c => c)
+            .ToArray();
+
+        if (string.IsNullOrEmpty(name))
+        {
+            await channel.SendWarningCardAsync("请设置绑定名称，举例：！组队 绑定 真格上分");
+        }
+        else
+        {
+            var cardBuilder = new CardBuilder();
+            // Header element
+            cardBuilder.AddModule<SectionModuleBuilder>(s =>
+            {
+                s.WithText($"""
+                            **欢迎使用组队绑定功能**
+                            > 您正在{(names.Contains(name) ? "重新" : "")}绑定 {name}
+                            > 请先加入任意语音频道，该频道将成为后续自动创建语音频道的入口
+                            > 加入后，请点击下方按钮
+                            """, true);
+            });
+            // Button element
+            cardBuilder.AddModule<ActionGroupModuleBuilder>(a =>
+            {
+                a.AddElement(b =>
+                {
+                    // Click this button will run DoBindingParentChannel with the name which user input
+                    b.WithText("我已加入语音频道")
+                        .WithClick(ButtonClickEventType.ReturnValue)
+                        .WithValue($"tp_binding_{name}")
+                        .WithTheme(ButtonTheme.Primary);
+                });
+            });
+
+            await channel.SendCardAsync(cardBuilder.Build());
+            Log.Info($"已发送绑定向导，目标类型：{name}");
+        }
+    }
+
+    private static async Task ListConfigs(SocketTextChannel channel)
+    {
+        await using var dbCtx = new DatabaseContext();
+        var configRecords = dbCtx.TpConfigs.OrderByDescending(e => e.Name).ToArray();
+        if (!configRecords.Any())
+        {
+            await channel.SendTextAsync("您还没有配置任何组队语音频道");
+        }
+        else
+        {
+            var configs = configRecords
+                .Select(e =>
+                    $"ID：{e.Id}，名称：{e.Name}，频道：{MentionUtils.KMarkdownMentionChannel(e.VoiceChannelId)}，" +
+                    $"语音质量：{VoiceQualityNames.Get(e.VoiceQuality)}，当前语音房间数：{e.RoomInstances.Count}"
+                )
+                .ToArray();
+            await channel.SendTextAsync(string.Join("\n", configs));
+        }
+    }
+
+    private static async Task BindingTextChannel(SocketTextChannel channel, string name)
+    {
+        await using var dbCtx = new DatabaseContext();
+        var config = dbCtx.TpConfigs.FirstOrDefault(e => e.Name == name);
+        if (config == null)
+        {
+            await channel.SendWarningCardAsync("指定配置不存在");
+        }
+        else
+        {
+            config.TextChannelId = channel.Id;
+            dbCtx.SaveChanges();
+            await channel.SendSuccessCardAsync(
+                $"绑定成功！在该频道使用组队指令将" +
+                $"自动在 {MentionUtils.KMarkdownMentionChannel(config.VoiceChannelId)} 下创建语音频道"
+            );
+        }
+    }
+
+    private static async Task SetDefaultQuality(SocketTextChannel channel, string msg)
+    {
+        var args = msg.Split(" ");
+        if (args.Length < 2)
+        {
+            await channel.SendWarningCardAsync("参数不足！举例：语音质量 1 高");
+            return;
+        }
+
+        var quality = VoiceQualityNames.FromName(args[1]);
+        if (quality == null)
+        {
+            await channel.SendWarningCardAsync("请从以下选项中选择语音质量：低，中，高");
+        }
+        else
+        {
+            if (!int.TryParse(args[1], out var configId))
+            {
+                await channel.SendWarningCardAsync("配置 ID 应为数字，举例：语音质量 1 高");
+            }
+            else
+            {
+                await using var dbCtx = new DatabaseContext();
+                var config = dbCtx.TpConfigs.FirstOrDefault(e => e.Id == configId);
+                if (config == null)
+                {
+                    await channel.SendWarningCardAsync("配置 ID 不存在，请使用：“！组队 列表”指令查看现有配置");
+                }
+                else
+                {
+                    config.VoiceQuality = (VoiceQuality)quality;
+                    dbCtx.SaveChanges();
+                    await channel.SendSuccessCardAsync("设置成功！");
+                }
+            }
+        }
+    }
+}
