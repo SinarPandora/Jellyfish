f358775bdb020ee08d13d11789996c0033558bbb
Sinar Pandora <SinarPandora@outlook.com>
2023-08-28 00:42:57 +0800

    Get role name from cache

--- Jellyfish/src/Module/Role/Data/UserRole.cs
+++ Jellyfish/src/Module/Role/Data/UserRole.cs
@@ -1,3 +1,6 @@
+using Kook.WebSocket;
+using LanguageExt;
+
 namespace Jellyfish.Module.Role.Data;
 
 /// <summary>
@@ -5,18 +8,38 @@ namespace Jellyfish.Module.Role.Data;
 /// </summary>
 public class UserRole
 {
-    public UserRole(string name, uint kookId, ulong guildId)
+    public UserRole(uint kookId, ulong guildId)
     {
-        Name = name;
         KookId = kookId;
         GuildId = guildId;
     }
 
     public long Id { get; set; }
-    public string Name { get; set; }
     public uint KookId { get; set; }
     public ulong GuildId { get; set; }
     public bool? Enabled { get; set; } = true;
 
     public ICollection<UserCommandPermission> CommandPermissions = new List<UserCommandPermission>();
+
+    /// <summary>
+    ///     Get kook role using cache from Kook client
+    /// </summary>
+    /// <param name="client">Kook client</param>
+    /// <returns>Socket role object</returns>
+    public Option<SocketRole> GetKookRole(KookSocketClient client) =>
+    (
+        from guild in client.Guilds
+        where guild.Id == GuildId
+        from role in guild.Roles
+        where role.Id == KookId
+        select role
+    ).FirstOrDefault();
+
+    /// <summary>
+    ///     Get role name using cache from Kook client
+    /// </summary>
+    /// <param name="client">Kook client</param>
+    /// <returns>Role name in guild</returns>
+    public string GetName(KookSocketClient client) =>
+        GetKookRole(client).Match(r => r.Name, "未找到");
 }
