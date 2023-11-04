630665cc7736f964dcf51d89a689a55adfe4f76f
Sinar Pandora <SinarPandora@outlook.com>
2023-11-04 14:33:55 +0800

    Fix duplicate message sending

--- Jellyfish/src/Module/TeamPlay/User/TeamPlayTeammateJoinCommand.cs
+++ Jellyfish/src/Module/TeamPlay/User/TeamPlayTeammateJoinCommand.cs
@@ -15,11 +15,14 @@ public class TeamPlayTeammateJoinCommand : UserConnectEventCommand
 {
     private readonly ILogger<TeamPlayTeammateJoinCommand> _log;
     private readonly DatabaseContext _dbCtx;
+    private readonly KookSocketClient _kook;
 
-    public TeamPlayTeammateJoinCommand(DatabaseContext dbCtx, ILogger<TeamPlayTeammateJoinCommand> log)
+    public TeamPlayTeammateJoinCommand(DatabaseContext dbCtx, ILogger<TeamPlayTeammateJoinCommand> log,
+        KookSocketClient kook)
     {
         _dbCtx = dbCtx;
         _log = log;
+        _kook = kook;
     }
 
     public override string Name() => "队友加入组队语音频道指令";
@@ -34,21 +37,23 @@ public class TeamPlayTeammateJoinCommand : UserConnectEventCommand
         if (room?.TmpTextChannel == null) return CommandResult.Continue;
 
         var tmpInstance = room.TmpTextChannel;
-        var textChannel = channel.Guild.GetTextChannel(tmpInstance.ChannelId);
-        if (textChannel == null)
+        var restGuild = await _kook.Rest.GetGuildAsync(channel.Guild.Id);
+        var restTextChannel = await restGuild.GetTextChannelAsync(tmpInstance.ChannelId);
+        if (restTextChannel == null)
         {
             room.TmpTextChannelId = null;
             _dbCtx.SaveChanges();
             _log.LogInformation("检测到文字房间已被删除，已删除组队房间中临时文字房间的绑定关系");
             _log.LogInformation("数据库中的临时文字房间实例将在稍后由扫描任务清理");
         }
-        else
+        else if (restTextChannel.GetPermissionOverwrite(user.Value) == null)
         {
-            await textChannel.OverrideUserPermission(user.Value, p => p.Modify(viewChannel: PermValue.Allow));
+            await restTextChannel.OverrideUserPermission(user.Value, p => p.Modify(viewChannel: PermValue.Allow));
             _log.LogInformation("加入组队房间 {TpRoomName} 的用户：{Name}:{Id}，已获得文字房间 {TextRoomName} 的访问权限",
-                room.RoomName, user.Value.DisplayName(), user.Id, textChannel.Name
+                room.RoomName, user.Value.DisplayName(), user.Id, restTextChannel.Name
             );
-            await textChannel.SendSuccessCardAsync($"欢迎 {MentionUtils.KMarkdownMentionUser(user.Id)} 加入组队房间！", false);
+            await restTextChannel.SendSuccessCardAsync($"欢迎 {MentionUtils.KMarkdownMentionUser(user.Id)} 加入组队房间！",
+                false);
         }
 
         return CommandResult.Done;
