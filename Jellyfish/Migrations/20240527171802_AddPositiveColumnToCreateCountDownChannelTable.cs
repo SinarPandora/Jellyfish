a666d1db47d2ea0e5267da0d8f0e182440c4e26e
Sinar Pandora <SinarPandora@outlook.com>
2024-05-28 01:18:18 +0800

    Add positive flag column

---
+++ Jellyfish/Migrations/20240527171802_AddPositiveColumnToCreateCountDownChannelTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPositiveColumnToCreateCountDownChannelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "positive",
+                table: "count_down_channels",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "positive",
+                table: "count_down_channels");
+        }
+    }
+}
