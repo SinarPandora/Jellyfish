b199d9d244e146d33f69b10dc149de17ad1a7cae
Sinar Pandora <SinarPandora@outlook.com>
2024-06-04 13:21:21 +0800

    Add given role id to the qualified history

---
+++ Jellyfish/Migrations/20240604052110_AddGivenRoleIdToQualifiedHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGivenRoleIdToQualifiedHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "given_role_id",
+                table: "clock_in_stage_qualified_histories",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "given_role_id",
+                table: "clock_in_stage_qualified_histories");
+        }
+    }
+}
--- Jellyfish/src/Module/ClockIn/Data/ClockInStageQualifiedHistory.cs
+++ Jellyfish/src/Module/ClockIn/Data/ClockInStageQualifiedHistory.cs
@@ -5,11 +5,12 @@ namespace Jellyfish.Module.ClockIn.Data;
 /// <summary>
 ///     History for user qualified
 /// </summary>
-public class ClockInStageQualifiedHistory(long stageId, long userStatusId)
+public class ClockInStageQualifiedHistory(long stageId, long userStatusId, uint? givenRoleId)
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public long StageId { get; init; } = stageId;
     public long UserStatusId { get; init; } = userStatusId;
+    public uint? GivenRoleId { get; set; } = givenRoleId;
     [Column(TypeName = "timestamp")] public DateTime CreateTime { get; init; } = DateTime.Now;
 
     // References
