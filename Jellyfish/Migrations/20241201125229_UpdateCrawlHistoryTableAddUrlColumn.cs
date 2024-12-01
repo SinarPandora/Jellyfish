55f784d39ca5abea3e05205cf917a1ed4bc6e98e
Sinar Pandora <SinarPandora@outlook.com>
2024-12-01 21:04:37 +0800

    Add URL to crawl history

---
+++ Jellyfish/Migrations/20241201125229_UpdateCrawlHistoryTableAddUrlColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCrawlHistoryTableAddUrlColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "url",
+                table: "weibo_crawl_histories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "url",
+                table: "weibo_crawl_histories");
+        }
+    }
+}
