03b24e0c4d9ebb7cf9a00954f84b5f45115ac845
Sinar Pandora <SinarPandora@outlook.com>
2025-05-31 18:22:56 +0800

    Add recall message table

---
+++ Jellyfish/Migrations/20250531102236_AddRecallMessageTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfish.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRecallMessageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "recall_messages",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    guild_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    channel_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    message_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    create_time = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    update_time = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_recall_messages", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "recall_messages");
+        }
+    }
+}
